27046075df1beb8b650591c66c419d517077b880
ömer faruk inel <omer.inel@ciellos.com>
2026-04-25 16:12:02 +0300

    v1.1.0

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "send2notion",
     fullname = _("Send to Notion"),
     description = _([[Send quick notes and highlights from KOReader to a Notion page. Supports multiple targets (different integrations/pages) switchable from the note screen, optional highlight-menu button, and automatic retry once the device comes online.]]),
-    version = "1.0.0",
+    version = "1.1.0",
 }
